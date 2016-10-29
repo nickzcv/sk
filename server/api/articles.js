127e4857b3a5be768ea58cd1f5d01f44e9d9eaa6
Nick Z <nickzcv@gmail.com>
2016-10-29 15:01:33 +0300

    update

---
+++ server/api/articles.js
@@ -0,0 +1,118 @@
+var express = require('express');
+var router = express.Router();              // get an instance of the express Router
+var getSlug = require('speakingurl');
+
+//Define mongoose Schema
+var Article = require('../models/article');
+
+
+// ----------------------------------------------------
+router.route('/news')
+
+	.post(function(req, res) {
+
+		var page = new Page();
+
+		page.category = req.body.category;
+		page.title = req.body.title;
+		page.url = getSlug( page.title );
+		page.description = req.body.description;
+		page.content = req.body.content;
+		page.img = req.body.img;
+		page.created_at = new Date();
+		page.updated_at = new Date();
+
+		page.isMain = req.body.isMain ? true : false;
+
+		// save and check for errors
+		page.save(function(err) {
+			if (err)
+				res.send(err);
+
+			res.json({ message: 'Page created!' });
+		});
+
+	})
+
+	.get(function(req, res) {
+
+		Page.find( {}, null, {sort: {created_at: -1}}, function(err, page) {
+			if (err) throw err;
+
+			res.render('api-pages', res.locals.template_data = {
+				layout: 'api',
+				active: { pages: true },
+				meta_title: 'Управление страницами сайта',
+				pages: page
+			});
+
+		});
+
+	});
+
+
+// ----------------------------------------------------
+router.route('/pages/:page_id')
+
+	.get(function(req, res) {
+		Page.findById(req.params.page_id, function(err, page) {
+			if (err)
+				res.send(err);
+
+			res.json(page);
+		});
+	})
+	// update
+	.put(function(req, res) {
+		Page.findById(req.params.page_id, function(err, page) {
+
+			if (err)
+				res.send(err);
+
+			page.category = req.body.category;
+			page.title = req.body.title;
+			page.url = getSlug( page.title );
+			page.description = req.body.description;
+			page.content = req.body.content;
+			page.img = req.body.img;
+			page.updated_at = new Date();
+
+			page.isMain = req.body.isMain ? true : false;
+
+
+			page.save(function(err) {
+				if (err)
+					res.send(err);
+
+				res.json({ message: 'Page updated!' });
+			});
+
+		});
+	})
+
+	// delete
+	.delete(function(req, res) {
+		Page.remove({
+			_id: req.params.page_id
+		}, function(err, page) {
+			if (err)
+				res.send(err);
+
+			res.json({ message: 'Successfully deleted' });
+		});
+	});
+
+
+router.route('/pages-total')
+
+	.get(function(req, res) {
+		Page.count({}, function( err, count){
+			if (err) throw err;
+
+			res.json(count);
+		})
+	});
+
+
+
+module.exports = router;
--- server/models/article.js
+++ server/models/article.js
@@ -1,15 +1,16 @@
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
-var PageSchema   = new Schema({
+var ArticleSchema   = new Schema({
 	category : { type: String, required: true},
 	title: { type: String, required: true},
 	url: { type: String, required: true, unique: true },
 	description:  String,
 	content:  String,
+	img: String,
 	created_at: Date,
 	updated_at: Date,
 	isMain: { type: Boolean, default: false }
 });
 
-module.exports = mongoose.model('Page', PageSchema);
+module.exports = mongoose.model('Article', ArticleSchema);
